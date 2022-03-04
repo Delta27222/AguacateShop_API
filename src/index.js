3709bd7466df9731ef91d2cd4e8cfc55823ba820
Angel <angelgabriel.hernandez@gmail.com>
2022-03-04 14:05:31 -0600

    Agregando evento a los h2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@
 
  const baseUrl = 'https://platzi-avo.vercel.app';
  const appNode = document.querySelector('#app'); //Esto lo usamos para que posicionemos en el HTML
+
+// ACA AGREGAMOS EL EVENTLISTENER para el nodo app
+appNode.addEventListener('click', (event) => {
+    if (event.target.nodeName === 'H2'){
+        window.alert('Este es el evento desde el nodeApp, desde el h2');
+    }
+})
+
+
+
  
  const formatPrice = (price) => {
      // Llamaremos a la API de Intercionalizacion, que trebaja con monedas y fechas
@@ -38,6 +48,12 @@
              const tittle = document.createElement('h2');
              tittle.textContent = item.name;
              tittle.className = 'text-lg';
+
+            //  Le agregamos un EVENTO
+            //con esto hacemos que se muestre un mensaje en pantalla cuando le demos a los titulos
+            // tittle.addEventListener('click', () => {
+            //     window.alert('Esto es un evento'); 
+            // })
                                                              // document.body.appendChild(tittle);
              // (c) creamos el precio
              const price = document.createElement('div');
